a7240b24971c17638926e6de5a3f195c887bd878
RCrobotcat <136684976+RCrobotcat@users.noreply.github.com>
2024-10-08 16:01:08 +0800

    Added user punch in management.

--- vue/src/views/manager/userPunchIn.vue
+++ vue/src/views/manager/userPunchIn.vue
@@ -6,60 +6,67 @@
     <!--      <el-button type="warning" plain style="margin-left: 10px" @click="reset">重置</el-button>-->
     <!--    </div>-->
 
-    <!--    <div class="operation">-->
-    <!--      <el-button type="primary" plain @click="handleAdd">新增</el-button>-->
-    <!--      <el-button type="danger" plain @click="delBatch">批量删除</el-button>-->
-    <!--    </div>-->
+    <div class="operation">
+      <el-button type="primary" plain @click="handleAdd">ADD</el-button>
+    </div>
 
     <div class="table">
-      <el-table :data="tableData" stripe  @selection-change="handleSelectionChange">
-        <el-table-column align="center" type="selection" width="55"></el-table-column>
+      <el-table :data="tableData" stripe>
+        <el-table-column align="center" width="55"></el-table-column>
         <el-table-column align="center" label="ID" prop="id" sortable width="80"></el-table-column>
         <el-table-column label="username" prop="username" show-overflow-tooltip></el-table-column>
         <el-table-column label="password" prop="password" show-overflow-tooltip></el-table-column>
         <el-table-column label="nickname" prop="nickname" show-overflow-tooltip></el-table-column>
         <el-table-column label="email" prop="email" show-overflow-tooltip></el-table-column>
-        <el-table-column label="Face Image" prop="faceImage"></el-table-column>
-        <el-table-column label="Punch In Time" prop="timestamp" show-overflow-tooltip></el-table-column>
         <el-table-column label="exp" prop="exp" show-overflow-tooltip></el-table-column>
+        <el-table-column label="face image" prop="face_image" show-overflow-tooltip></el-table-column>
 
         <el-table-column align="center" label="Actions" width="180">
           <template v-slot="scope">
-            <el-button plain size="mini" type="primary" @click="handleEdit(scope.row)">Edit</el-button>
-            <el-button plain size="mini" type="danger" @click=del(scope.row.id)>Delete</el-button>
+            <el-button plain size="mini" type="danger" @click=del(scope.row.username)>Delete</el-button>
           </template>
         </el-table-column>
       </el-table>
 
-<!--      <div class="pagination">-->
-<!--        <el-pagination-->
-<!--            :current-page="pageNum"-->
-<!--            :page-size="pageSize"-->
-<!--            :page-sizes="[5, 10, 20]"-->
-<!--            :total="total"-->
-<!--            background-->
-<!--            layout="total, prev, pager, next"-->
-<!--            @current-change="handleCurrentChange">-->
-<!--        </el-pagination>-->
-<!--      </div>-->
+      <el-dialog :close-on-click-modal="false" :visible.sync="fromVisible" destroy-on-close title="Info" width="40%">
+        <el-form ref="formRef" :model="form" :rules="rules" label-width="100px" style="padding-right: 50px">
+          <el-form-item label="ID" prop="id">
+            <el-input v-model="form.id" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="username" prop="username">
+            <el-input v-model="form.username" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="nickname" prop="nickname">
+            <el-input v-model="form.nickname" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="email" prop="email">
+            <el-input v-model="form.email" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="password" prop="password">
+            <el-input v-model="form.password" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="exp" prop="exp">
+            <el-input v-model="form.exp" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="Face Image" prop="face_image">
+            <el-upload
+                class="avatar-uploader"
+                :action="$baseUrl + '/image/'"
+                :show-file-list="false"
+                :on-success="handleAvatarSuccess"
+            >
+              <img v-if="form.face_image" :src="form.face_image" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="fromVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="save">Save</el-button>
+        </div>
+      </el-dialog>
+
     </div>
-
-
-      <!--    <el-dialog title="信息" :visible.sync="fromVisible" width="40%" :close-on-click-modal="false" destroy-on-close>-->
-      <!--      <el-form label-width="100px" style="padding-right: 50px" :model="form" :rules="rules" ref="formRef">-->
-      <!--        <el-form-item prop="title" label="标题">-->
-      <!--          <el-input v-model="form.title" autocomplete="off"></el-input>-->
-      <!--        </el-form-item>-->
-      <!--        <el-form-item prop="content" label="内容">-->
-      <!--          <el-input type="textarea" :rows="5" v-model="form.content" autocomplete="off"></el-input>-->
-      <!--        </el-form-item>-->
-      <!--      </el-form>-->
-      <!--      <div slot="footer" class="dialog-footer">-->
-      <!--        <el-button @click="fromVisible = false">取 消</el-button>-->
-      <!--        <el-button type="primary" @click="save">确 定</el-button>-->
-      <!--      </div>-->
-      <!--    </el-dialog>-->
-
   </div>
 </template>
 
@@ -69,108 +76,85 @@ export default {
   data() {
     return {
       tableData: [],  // 所有的数据
-      pageNum: 1,   // 当前的页码
-      pageSize: 10,  // 每页显示的个数
-      total: 0,
-      title: null,
       fromVisible: false,
       form: {},
-      // user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
-      // rules: {
-      //   title: [
-      //     {required: true, message: '请输入标题', trigger: 'blur'},
-      //   ],
-      //   content: [
-      //     {required: true, message: '请输入内容', trigger: 'blur'},
-      //   ]
-      // },
-      ids: []
+      rules: {
+        id: [
+          {required: true, message: 'please enter content', trigger: 'blur'},
+        ],
+        username: [
+          {required: true, message: 'please enter content', trigger: 'blur'},
+        ],
+        password: [
+          {required: true, message: 'please enter content', trigger: 'blur'},
+        ],
+        nickname: [
+          {required: true, message: 'please enter content', trigger: 'blur'},
+        ],
+        email: [
+          {required: true, message: 'please enter content', trigger: 'blur'},
+        ],
+        exp: [
+          {required: true, message: 'please enter content', trigger: 'blur'},
+        ],
+        face_image: [
+          {required: true, message: 'please upload image', trigger: 'blur'},
+        ],
+      },
     }
   },
   created() {
-    // this.load(1)
+    this.loadTableData();
   },
   methods: {
     handleEdit(row) {   // 编辑数据
       this.form = JSON.parse(JSON.stringify(row))  // 给form对象赋值  注意要深拷贝数据
       this.fromVisible = true   // 打开弹窗
     },
-    handleSelectionChange(rows) {   // 当前选中的所有的行数据
-      this.ids = rows.map(v => v.id)   //  [1,2]
+    loadTableData(){
+      this.$request.get('/user/').then(res => {
+        this.tableData = res || []
+      })
     },
-    // delBatch() {   // 批量删除
-    //   if (!this.ids.length) {
-    //     this.$message.warning('请选择数据')
-    //     return
-    //   }
-    //   this.$confirm('您确定批量删除这些数据吗？', '确认删除', {type: "warning"}).then(response => {
-    //     this.$request.delete('/notice/delete/batch', {data: this.ids}).then(res => {
-    //       if (res.code === '200') {   // 表示操作成功
-    //         this.$message.success('操作成功')
-    //         this.load(1)
-    //       } else {
-    //         this.$message.error(res.msg)  // 弹出错误的信息
-    //       }
-    //     })
-    //   }).catch(() => {
-    //   })
-    // },
-    // load(pageNum) {  // 分页查询
-    //   if (pageNum) this.pageNum = pageNum
-    //   this.$request.get('/notice/selectPage', {
-    //     params: {
-    //       pageNum: this.pageNum,
-    //       pageSize: this.pageSize,
-    //       title: this.title,
-    //     }
-    //   }).then(res => {
-    //     this.tableData = res.data?.list
-    //     this.total = res.data?.total
-    //   })
-    // },
-    // reset() {
-    //   this.title = null
-    //   this.load(1)
-    // },
-    // handleCurrentChange(pageNum) {
-    //   this.load(pageNum)
-    // },
-    // save() {   // 保存按钮触发的逻辑  它会触发新增或者更新
-    //   this.$refs.formRef.validate((valid) => {
-    //     if (valid) {
-    //       this.$request({
-    //         url: this.form.id ? '/notice/update' : '/notice/add',
-    //         method: this.form.id ? 'PUT' : 'POST',
-    //         data: this.form
-    //       }).then(res => {
-    //         if (res.code === '200') {  // 表示成功保存
-    //           this.$message.success('保存成功')
-    //           this.load(1)
-    //           this.fromVisible = false
-    //         } else {
-    //           this.$message.error(res.msg)  // 弹出错误的信息
-    //         }
-    //       })
-    //     }
-    //   })
-    // },
-    // del(id) {   // 单个删除
-    //   this.$confirm('您确定删除吗？', '确认删除', {type: "warning"}).then(response => {
-    //     this.$request.delete('/notice/delete/' + id).then(res => {
-    //       if (res.code === '200') {   // 表示操作成功
-    //         this.$message.success('操作成功')
-    //         this.load(1)
-    //       } else {
-    //         this.$message.error(res.msg)  // 弹出错误的信息
-    //       }
-    //     })
-    //   }).catch(() => {
-    //   })
-    // },
-    // handleAdd() {   // 新增数据
-    //   this.form = {}  // 新增数据的时候清空数据
-    //   this.fromVisible = true   // 打开弹窗
-    // },
+    handleAdd() {   // 新增数据
+      this.form = {}  // 新增数据的时候清空数据
+      this.fromVisible = true   // 打开弹窗
+    },
+    save() {
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          this.$request.post('/user/', this.form).then(res => {
+            if (res.message === 'User created successfully.') {
+              this.$message.success('User created successfully.');
+              this.loadTableData();
+              this.fromVisible = false;
+            } else if (res.detail && res.detail.length > 0) {
+              this.$message.error(res.detail[0].msg);
+            }
+          }).catch(error => {
+            this.$message.error('An error occurred while creating the user.');
+          });
+        }
+      });
+    },
+    del(username){
+      this.$confirm('Are you sure you want to delete it?', 'Confirm deletion', {type: "warning"}).then(response => {
+        this.$request.delete('/user/' + username).then(res => {
+          if (!res) {
+            this.$message.success('User deleted successfully.');
+            this.loadTableData();
+          } else {
+            this.$message.error('An error occurred while deleting the user.');
+          }
+        }).catch(error => {
+          this.$message.error('An error occurred while deleting the user.');
+        });
+      }).catch(() => {
+      });
+    },
+    handleAvatarSuccess(response, file, fileList) {
+      this.$set(this.tableData, 'face_image', response)
+    },
   }
 }
 </script>
